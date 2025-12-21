5ab32ae39158138e1fb2147df5e5094c30457259
azabla <andualemchanie.y@gmail.com>
2025-12-21 15:49:53 +0300

    small fix

--- app/Filament/Resources/ExpenseCategoryResource.php
+++ app/Filament/Resources/ExpenseCategoryResource.php
@@ -18,6 +18,8 @@ class ExpenseCategoryResource extends Resource
     protected static ?string $model = ExpenseCategory::class;
 
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationGroup = 'Expense Management';
+
 
     public static function form(Form $form): Form
     {
--- app/Filament/Resources/ExpenseResource.php
+++ app/Filament/Resources/ExpenseResource.php
@@ -19,6 +19,8 @@ class ExpenseResource extends Resource
 
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
 
+    protected static ?string $navigationGroup = 'Expense Management';
+
     public static function form(Form $form): Form
     {
         return $form
