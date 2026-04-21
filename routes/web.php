8a5a1a3e6736f5edb7cfcd698040ef249dd2686d
azabla <andualemchanie.y@gmail.com>
2026-04-21 11:30:44 +0300

    start deploying

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,22 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Artisan;
 use Livewire\Volt\Volt;
 
+Route::get('/run-migrations/{password}', function ($password) {
+    // Replace 'mysecret123' with a password of your choice
+    if ($password !== 'secret123o') {
+        return response('Unauthorized.', 403);
+    }
 
+    try {
+        // This runs 'php artisan migrate --force'
+        // The --force is REQUIRED in production/hosting
+        Artisan::call('migrate', ['--force' => true]);
+        
+        return "Migrations successful! <br><br> Output: <br>" . Artisan::output();
+    } catch (\Exception $e) {
+        return "Error: " . $e->getMessage();
+    }
+});
 
 Route::get('/', function () {
     return view('welcome');
